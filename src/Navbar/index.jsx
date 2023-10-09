b2da783e33191a08491d6a8ebc0ff25b1e7f7d75
Pontes <hpo.pontes@gmail.com>
2023-10-09 19:36:20 -0300

    adicionando sass no Navbar, conponents, footer e index; (falta paginas do site)

--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -1,7 +1,7 @@
 import {React, useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 
-import '../Navbar/Navbar.css'
+import '../Navbar/Navbar.scss'
 
 function Navbar() {
   const initialDarkMode = localStorage.getItem('isDarkMode') === 'true';
--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Button.css";
+import "./Button.scss";
 
 const Button = ({ Text, onClick, Type = "button" }) => {
   return (
